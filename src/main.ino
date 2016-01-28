9eb57a4da27241dae475b163adb3469ece7a98ab
Helber Maciel Guerra <helber@cianet.ind.br>
2016-01-28 11:03:01 -0200

    Recebendo e enviando json

--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,5 @@
 #include <NewPing.h>
+#include <SoftwareSerial.h>
 
 #define MAX_DISTANCE   200
 
@@ -6,25 +7,50 @@
 #define ECHO_PIN1       11
 #define TRIGGER_PIN2     8
 #define ECHO_PIN2        9
-
+#define ESP_TX           3
+#define ESP_RX           2
 
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+SoftwareSerial esp_serial(ESP_RX, ESP_TX); // RX, TX
 
 void setup() {
   Serial.begin(115200);
+  esp_serial.begin(9600);
   Serial.println("Iniciado...");
 };
 
+String get_json() {
+    String ret = "";
+    int d1, d2;
+    delay(100);
+    d1 = sonar1.ping_cm();
+    delay(100);
+    d2 = sonar2.ping_cm();
+    ret += "{\"sonar1\": ";
+    ret += d1;
+    ret += ", \"sonar2\": ";
+    ret += d2;
+    ret += "}";
+    return ret;
+}
+
+
 void loop() {
-  int d1, d2;
-  delay(100);
-  d1 = sonar1.ping_cm();
-  delay(100);
-  d2 = sonar2.ping_cm();
-  Serial.print("{\"sonar1\": ");
-  Serial.print(d1);
-  Serial.print(", \"sonar2\": ");
-  Serial.print(d2);
-  Serial.print("}\r\n");
+    String input = "";
+    while (esp_serial.available() > 0) {
+        input += (char) esp_serial.read();
+        // delay(5); // Nao sei se e necessario
+    }
+
+    if (input == "GET\n") {
+        Serial.println("Recebeu GET... enviando");
+        String json = get_json();
+        esp_serial.println(json);
+        Serial.println(json);
+    }
+
+    if (input == "\n") {
+        Serial.println("Recebeu newline");
+    }
 };
