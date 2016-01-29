6eac5d25e7097ca87942ef222a893d49e6f5a4ea
Helber Maciel Guerra <helber@cianet.ind.br>
2016-01-29 15:44:40 -0200

    working

--- src/main.ino
+++ src/main.ino
@@ -7,8 +7,8 @@
 #define ECHO_PIN1       11
 #define TRIGGER_PIN2     8
 #define ECHO_PIN2        9
-#define ESP_TX           3
-#define ESP_RX           2
+#define ESP_TX           6
+#define ESP_RX           7
 
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
@@ -16,8 +16,16 @@ SoftwareSerial esp_serial(ESP_RX, ESP_TX); // RX, TX
 
 void setup() {
   Serial.begin(115200);
-  esp_serial.begin(9600);
+  esp_serial.begin(115200);
   Serial.println("Iniciado...");
+  /*
+  pinMode(ESP_TX, OUTPUT);
+  pinMode(ESP_RX, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(ESP_TX, LOW);
+  digitalWrite(ESP_RX, LOW);
+  digitalWrite(13, LOW);
+  */
 };
 
 String get_json() {
@@ -34,23 +42,39 @@ String get_json() {
     ret += "}";
     return ret;
 }
-
+/*
+void loop() {
+    Serial.println("Vivo...");
+    digitalWrite(ESP_TX, LOW);
+    digitalWrite(ESP_RX, LOW);
+    digitalWrite(13, LOW);
+    delay(1000);
+    digitalWrite(ESP_TX, HIGH);
+    digitalWrite(ESP_RX, HIGH);
+    digitalWrite(13, HIGH);
+    delay(1000);
+}
+*/
 
 void loop() {
     String input = "";
     while (esp_serial.available() > 0) {
         input += (char) esp_serial.read();
-        // delay(5); // Nao sei se e necessario
+        delay(5); // Nao sei se e necessario
     }
 
-    if (input == "GET\n") {
+    if (input == "SENSOR") {
         Serial.println("Recebeu GET... enviando");
         String json = get_json();
         esp_serial.println(json);
         Serial.println(json);
     }
 
-    if (input == "\n") {
-        Serial.println("Recebeu newline");
+    if (input == "GET /") {
+        Serial.println("Caregou /");
+    }
+    if (input != ""){
+        Serial.print(" > ");
+        Serial.println(input);
     }
 };
